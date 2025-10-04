9dcfc7621d074f10f83d9642f5bf7cb6c64e0dac
yui-szk <yuisuzuki.017@gmail.com>
2025-10-04 19:47:08 +0900

    Remove unused Link import from App component

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TopPage from './TopPage'; 
 import MuseumScreen from "./components/MuseumScreen";
 
