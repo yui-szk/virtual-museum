203a7fae394b491b2e4ccfdc24d88d7053a6629c
yui-szk <yuisuzuki.017@gmail.com>
2025-10-04 19:36:32 +0900

    Refactor App component layout for improved styling and responsiveness

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <>
       <BrowserRouter>
-        <div className="App">
+        <div className="h-screen bg-gradient-to-b overflow-y-hidden from-gray-50 to-gray-100 flex flex-col items-center justify-center p-4">
           <Routes>
             <Route path="/" element={<TopPage />} />
             <Route path="/view" element={<MuseumScreen />} />
