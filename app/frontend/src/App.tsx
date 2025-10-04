e49e80e51df62dd45bbfa5b5f0bc83be025c4bc9
yui-szk <yuisuzuki.017@gmail.com>
2025-10-04 17:43:16 +0900

    use React Router for navigation

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import TopPage from './TopPage'; 
+import MuseumScreen from "./components/MuseumScreen";
 
 export default function App() {
   return (
-    // 初期でTopPageコンポーネントを表示
-    <TopPage />
+    <>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<TopPage />} />
+            <Route path="/view" element={<MuseumScreen />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
