eca89585c9faf17cb767bf323b6aec5f43f7524d
yui-szk <yuisuzuki.017@gmail.com>
2025-10-05 11:34:37 +0900

    feat: Implement MuseumScreenMe and enhance MuseumScreen with dynamic data fetching

--- app/frontend/src/components/MuseumPicture.tsx
+++ app/frontend/src/components/MuseumPicture.tsx
@@ -1,15 +1,13 @@
-import museumRoom from "../assets/museum-sample.jpg"
-
-const MuseumPicture = () => {
-  return (
-    <div className="w-full h-full overflow-hidden">
-      <img
-        src={museumRoom}
-        alt="Museum Room"
-        className="w-full h-full object-cover min-w-4xl"
-      />
-    </div>
-  );
+interface MuseumPictureProps {
+  imageUrl: string
 }
 
-export default MuseumPicture;
+const MuseumPicture = ({ imageUrl }: MuseumPictureProps) => {
+  return (
+    <div className="w-full h-full overflow-hidden">
+      <img src={imageUrl} alt="Museum Room" className="w-full h-full object-cover min-w-4xl" />
+    </div>
+  )
+}
+
+export default MuseumPicture
