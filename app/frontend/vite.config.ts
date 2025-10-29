f115812311a75989d200b8edf801282961d9943e
yui-szk <148746551+yui-szk@users.noreply.github.com>
2025-10-29 14:12:06 +0900

    Revert "GitHub Actionsを使用してGitHub Pagesに自動デプロイ"

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/virtual-museum/' : '/',
   server: {
     host: true,
     port: 5173,
